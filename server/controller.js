900e68376589cb5be1842481d442d8bcffecd736
Sam Pool <sambpool@gmail.com>
2021-10-12 17:11:05 -0600

    add log tagging to clock in

--- server/controller.js
+++ server/controller.js
@@ -4,31 +4,14 @@ const tags = require('../db/models/logtags_model');
 module.exports = {
 
   clockIn: function clockIn(req, res, next){
-
-    timelog.clockIn(req.params.userId, req.body.rate / 100 || 5000)
-    .then( insertId => {
-
-      tags.queryAllByUser(req.params.userId)
-      .then( userTags =>{
-
-        Promise.all(
-          req.body.tags.map( clockTag =>{
-
-            let preTag = userTags.find( userTag =>{
-              return userTag.tag_name === clockTag;
-            });
-            if (preTag){
-              tags.tagByTagId(insertId, preTag.tag_id);
-            } else {
-
-            }
-
-          })
-        )
-
-      });
-    })
-    .catch( err => console.log(err) );
+    return new Promise( (resolve, reject) =>{
+      let userId = Number(req.params.userId);
+      timelog.clockIn(userId, (req.body.rate / 100) || 5000)
+      .then( insertId => {
+        tags.tagLog(userId, insertId, req.body.tags)
+      }).then( resolve )
+      .catch( reject );
+    });
   },
 
   clockOut: function clockOut(req, res, next){
