3f4c625a3c35dcc4b4ec4c1ffdcf16d6a60096c5
Sam Pool <sambpool@gmail.com>
2021-10-13 08:31:24 -0600

    add clock API calls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,27 +9,63 @@ import {LogEntry} from "./components/LogEntry/LogEntry.jsx";
 import './App.css';
 
 export const App = props => {
+
+  const [clockedIn, setClockedIn] = useState(false);
   const [userId, setUserId] = useState(1);
   const [tags, setTags] = useState([]);
   const [log, setLog] = useState([]);
+
   function fetchLog(){
     axios.get(`/api/users/${userId}/log`)
-    .then( ({data}) => setLog( data ) )
+    .then( ({data}) => {
+      setLog( data.reverse() );
+      setClockedIn( data.some( logEntry => !logEntry.outtime ) );
+    })
     .catch( err => console.log(err) );
   }
+
+  function clockIn(){
+    axios.post(`/api/users/${userId}/log`)
+    .then( ({data}) =>{
+      console.log(data);
+      setClockedIn(true);
+      fetchLog();
+    });
+  }
+
+  function clockOut(){
+    axios.put(`/api/users/${userId}/log`)
+    .then( ({data}) =>{
+      console.log(data);
+      setClockedIn(false);
+      fetchLog();
+    })
+    .catch( err => console.log(err) );
+  }
+
+  function toggleClock(){
+    if (clockedIn){ clockOut() }
+    else { clockIn() }
+  }
+
   useEffect( ()=>{
     fetchLog();
   }, []);
+
   return (
     <div id='App'>
-      <button>Clock In</button>
+      <div id='clock'>
+        <button onClick={toggleClock}>
+          {clockedIn ? 'Clock Out' : 'Clock In'}
+        </button>
+      </div>
       <div id='log'>
         {
           log.map( entry =>{
             return(
               <LogEntry
                 entry={entry}
-                key={`log-entry-${entry.key}`}
+                key={`log-entry-${entry.id}`}
               />
               )
           })
