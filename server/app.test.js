679a9e62162ba3b2f32c872f489256c5e168a080
Sam Pool <sambpool@gmail.com>
2021-10-13 08:36:55 -0600

    refactor async/await tests

--- server/app.test.js
+++ server/app.test.js
@@ -2,20 +2,33 @@ const request = require('supertest');
 const {app} = require('./app');
 
 describe('API', ()=>{
+/*
+  describe('CLOCK-IN w/ tags', ()=>{
 
-  describe('CLOCK-IN', ()=>{
-    let response;
-    beforeAll( async()=>{
-      response = await request(app)
+    it('should return something', async ()=>{
+      let response = await request(app)
       .post('/api/users/1/log')
       .send({
         tags: ['tagOne', 'tagTwo', 'tagThree']
       })
-      .end( (err,res) =>{
-
-      });
+      console.log(response)
     });
-    it('should return something', ()=>{ console.log(response) });
+
+  });
+*/
+  describe('CLOCK-IN w/o tags', ()=>{
+
+    it('should return something', ()=>{
+      return request(app)
+      .post('/api/users/1/log')
+      .send({
+        tags: []
+      })
+        .then( response =>{
+          console.log( response );
+        });
+    });
+
   });
 
 });
