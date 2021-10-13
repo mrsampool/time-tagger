9cddbc577b12675f05463955e81ea10b1c23857b
Sam Pool <sambpool@gmail.com>
2021-10-13 13:34:58 -0600

    add prod logging for debugging

--- server/app.js
+++ server/app.js
@@ -2,23 +2,22 @@
 const express = require('express');
 const path = require('path');
 const { apiRouter } = require('./apiRouter');
-if (process.env.ENV !== 'PROD'){
+if (process.env.NODE_ENV !== 'production'){
   require('dotenv').config();
 }
-
-// Server Config
 const app = express();
-module.exports.app = app;
+app.use( express.static( path.join(__dirname, '..', 'client', 'build') ) );
 app.use( express.json() );
 
 // Serving
 app.use( (req, res, next) => {
-  if (process.env.ENV === 'DEV'){
+//  if (process.env.ENV === 'DEV'){
     console.log(`\nIncoming ${req.method} request to ${req.path}`);
     console.log(`Request Body:`)
     console.log(req.body);
     next();
-  }
+//  }
 });
-app.use( express.static( path.join(__dirname, '..', 'client', 'build') ) );
 app.use('/api', apiRouter);
+
+module.exports.app = app;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const {app} = require('./app');
-if (process.env.ENV !== 'PROD'){
+if (process.env.NODE_ENV !== 'production'){
   require('dotenv').config();
 }
 
-console.log(process.env);
-
 app.listen(process.env.PORT, ()=> {
-  if (process.env.ENV === 'DEV'){
     console.log(`\nI'm listening on PORT ${process.env.PORT}! 👂\n`);
+  if (process.env.NODE_ENV === 'production'){
   }
 });
