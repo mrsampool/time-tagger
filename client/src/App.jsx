57599ec364212e4a0b195a8b4b83aced687e60fe
Sam Pool <sambpool@gmail.com>
2021-10-13 07:17:49 -0600

    initial app structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+// Libraries
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+// Sub-Components
+import {LogEntry} from "./components/LogEntry/LogEntry.jsx";
+
+// Style Sheet
+import './App.css';
 
 export const App = props => {
+  const [userId, setUserId] = useState(1);
+  const [tags, setTags] = useState([]);
+  const [log, setLog] = useState([]);
+  function fetchLog(){
+    axios.get(`/api/users/${userId}/log`)
+    .then( ({data}) => setLog( data ) )
+    .catch( err => console.log(err) );
+  }
+  useEffect( ()=>{
+    fetchLog();
+  }, []);
   return (
-    <div>Hello World!</div>
+    <div id='App'>
+      <button>Clock In</button>
+      <div id='log'>
+        {
+          log.map( entry =>{
+            return(
+              <LogEntry
+                entry={entry}
+                key={`log-entry-${entry.key}`}
+              />
+              )
+          })
+        }
+      </div>
+    </div>
   )
 }
