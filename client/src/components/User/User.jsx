8a05bb1f554aba0c039d6e61f1406b2be707b09b
Sam Pool <sampool@Sams-MBP.fios-router.home>
2021-11-22 13:52:32 -0500

    add logout

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 import './User.css';
 
-export const User = ({ user }) => (
-    <div id={'User'}>
-        <span>{user.first_name}</span>
-    </div>
-)
+export const User = ({ user, logOut }) => {
+    function handleClick(){
+        logOut();
+    }
+    return (
+        <div id={'User'}>
+            <span>{user.first_name}</span>
+            <button onClick={handleClick}>
+                X
+            </button>
+        </div>
+    )
+}
